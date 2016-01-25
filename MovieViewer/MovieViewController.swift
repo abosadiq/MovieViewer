85ff4ad393f9b4253c05a65155b69e4f9dbda24c
abosadiq <wafimohamed@Wafis-MBP.att.net>
2016-01-24 18:22:28 -0800

    edit MovieViewer.xcworkspace

--- MovieViewer/MovieViewController.swift
+++ MovieViewer/MovieViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 import AFNetworking
+import BXProgressHUD
 
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate,UISearchDisplayDelegate{
+class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,
+UISearchBarDelegate{
+  
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet weak var tableView: UITableView!
     var refrechController = UIRefreshControl()
     var movies: [NSDictionary]?
+    var filteredData: [NSDictionary]!
     override func viewDidLoad() {
         super.viewDidLoad()
+//           let refreshControl = UIRefreshControl()
+//        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+       refrechController.backgroundColor = UIColor.grayColor()
+        refrechController.tintColor = UIColor.blueColor()
+        refrechController.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refrechController, atIndex: 0)
+       searchBar.delegate = self
+        filteredData = movies
+        var getView: UIView {
+            return self.view
+        }
         //var tableView = tableView
           tableView.dataSource = self
           tableView.delegate = self 
@@ -27,7 +44,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
             delegate:nil,
             delegateQueue:NSOperationQueue.mainQueue()
         )
-        
+         BXProgressHUD.showHUDAddedTo(getView).hide(afterDelay: 2)
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
                 if let data = dataOrNil {
@@ -36,7 +53,10 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
                             print("response: \(responseDictionary)")
                             
                             self.movies = responseDictionary["results"] as? [NSDictionary]
+                            self.filteredData = self.movies
+                        self.refrechController.endRefreshing()
                             self.tableView.reloadData()
+                        
                             
                     }
                 }
@@ -53,30 +73,38 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
     }
     
+    
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
 
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if let movies = movies {
-            return movies.count
-        } else{
+        if let filteredData = filteredData {
+            return filteredData.count
+        } else {
             return 0
         }
-       
+        //return filteredData?.count ?? 0
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-         let movie = movies! [indexPath.row]
-         let title = movie ["title"] as! String
-          let overview = movie ["overview"] as! String
-           let posterpath = movie ["poster_path"] as! String
+    
+         //let movie = movies! [indexPath.row]
+        let movie = filteredData![indexPath.row]
+        let title = movie ["title"] as! String
+        let overview = movie ["overview"] as! String
+        let posterpath = movie ["poster_path"] as! String
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         let imageUrl = NSURL(string: baseUrl + posterpath)
         
@@ -87,8 +115,41 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         print("(indexPath.row)")
         return cell
         }
-
-
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? movies : movies!.filter({ (movie: NSDictionary) -> Bool in
+            return (movie["title"] as! String).rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+         self.tableView.reloadData()
+    }
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (data, response, error) in
+            
+            // ... Use the new data to update the data source ...
+            
+            // Reload the tableView now that there is new data
+            self.tableView.reloadData()
+            
+            // Tell the refreshControl to stop spinning
+            refreshControl.endRefreshing()	
+            });
+        task.resume()
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ MovieViewer/MoviesNames.swift
@@ -0,0 +1,19 @@
+//
+//  MoviesNames.swift
+//  MovieViewer
+//
+//  Created by Wafi MoHamed on 1/22/16.
+//  Copyright © 2016 wafi. All rights reserved.
+//
+
+import Foundation
+
+struct MoviesNames
+{
+    let id: Int?
+    let title: String?
+    let overview: String?
+    let posterPath: String?
+    let voteAverage: Float?
+    let releaseDate: NSDate?
+}
